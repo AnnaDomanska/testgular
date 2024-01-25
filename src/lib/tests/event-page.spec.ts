d288d3d4d8b9faf41e78a73baa32d5cd2bbd09a0
annah <anna.h.domanska@gmail.com>
2024-01-25 20:49:50 +0100

    small fix

--- src/lib/tests/event-page.spec.ts
+++ src/lib/tests/event-page.spec.ts
@@ -52,28 +52,6 @@ it('navigation with eventId', APP_CONFIG, async (app: App) => {
   await header.expectContent(ANY_SIGN);
 });
 
-it('cast section', APP_CONFIG, async(app: App) => {
-  const router = app.inject(Router);
-  const el = app.inject(ElementLocator);
-
-  await router.navigate(
-    `/wydarzenia/${TEST_CONFIG.showSlug}?eventId=${TEST_CONFIG.eventId}`
-  );
-
-const castSection = el.locateChild(ContainerElement, cssSelector('section.cast'));
-const castList = await castSection.elementLocator.locateList(ClickableImageRow, cssSelector('km-clickable-image-row'));
-await castList.expectToHaveLength(5);
-await castList.forEachChild(castElement => castElement.expectData(UrlMatcher.startsWith('/artysta/', '')));
-const firstElement = await castList.getNthElement(0);
-const firstElementTitle = await firstElement.title.getContent();
-await firstElement.click();
-await router.expectAndWaitForUrl(UrlMatcher.startsWith('artysta/', ''));
-await app.waitForTimeout(5000);
-const mainSection = el.locateChild(ContainerElement, cssSelector('article'))
-const header = mainSection.elementLocator.locateChild(TextElement, cssSelector('h1'));
-await header.expectContent(StringMatcher.mustContainWords([firstElementTitle]));
-});
-
 it('reviews section', APP_CONFIG, async(app: App) => {
   const router = app.inject(Router);
   const el = app.inject(ElementLocator);
